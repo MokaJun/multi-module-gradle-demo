3eba6d27f786230b4ce6bdbdbdceeea85972ab46
moka <1296939448@qq.com>
2022-12-22 20:01:35 +0800

    feat: init by spring initializer

---
+++ src/main/java/com/zhibin/multimodulegradledemo/MultiModuleGradleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zhibin.multimodulegradledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleGradleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiModuleGradleDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhibin/multimodulegradledemo/MultiModuleGradleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhibin.multimodulegradledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleGradleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
